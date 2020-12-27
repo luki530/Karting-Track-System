1263687532fdc7cc14abe0e93f9983226c86bfb4
Michal <zienowit@gmail.com>
2020-12-27 16:40:10 +0100

    changes to database

--- aghart/views.py
+++ aghart/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from aghart.models import KartModel
+from django.db import connection
 # Create your views here.
 models = []
-models = [p.name for p in KartModel.objects.raw('SELECT id,name FROM kart_model')]
+def my_custom_sql(self):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT name FROM kart_model")
+        models = [item[0] for item in cursor.fetchall()]
+    return models
 
 def home(request):
     return render(request, 'aghart/home.html')
 
 def records(request):
-    return render(request, 'aghart/records.html', {'models': models})
+    return render(request, 'aghart/records.html', {'models': my_custom_sql(models)})
 
 def statistics(request):
     return render(request, 'aghart/statistics.html')
+
