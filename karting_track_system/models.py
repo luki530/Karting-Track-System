267725256e4d94678cfbdb2bdcc94c97ea551249
luki530 <lukasz.oszczypala@gmail.com>
2021-01-12 16:42:40 +0100

    changes

--- karting_track_system/models.py
+++ karting_track_system/models.py
@@ -6,9 +6,11 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=45)
     client_number = models.CharField(max_length=45)
--- karting_track_system/settings.py
+++ karting_track_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'karting_track_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
