57298eec18feb9f1c7be2a7b5aebc0d08f8e2511
Michal <zienowit@gmail.com>
2021-01-14 17:04:10 +0100

    settings

--- karting_track_system/settings.py
+++ karting_track_system/settings.py
@@ -132,14 +132,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = '/public_html/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/public_html/'
 
-STATIC_URL = '/public_html/'
-STATICFILES_DIRS = [
-   BASE_DIR / "public_html",
-   '/public_html',
-]
+# STATIC_URL = '/public_html/'
+# STATICFILES_DIRS = [
+#    BASE_DIR / "public_html",
+#    '/public_html',
+# ]
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
