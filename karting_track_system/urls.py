44a47861d4e6c0b548652ccc9ae9634af3b6209d
luki530 <lukasz.oszczypala@gmail.com>
2021-01-16 15:36:34 +0100

    oauth2 fb/github login
    * login with fb and github
    
    * Update urls.py
    
    * Update urls.py
    
    Co-authored-by: Michal <zienowit@gmail.com>
    Co-authored-by: MichalMaczek <44197726+MichalMaczek@users.noreply.github.com>

--- karting_track_system/urls.py
+++ karting_track_system/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('userprofile/', views.userProfile, name = 'userprofile'),
     path('control_races/', views.control_races, name= 'control_races'),
     re_path(r'^' + str(WEBHOOK_ENDPOINT) + '/$', FacebookWebhookView.as_view(), name='webhook'),
-    path('policy/',views.policy, name = 'policy')
+    path('policy/',views.policy, name = 'policy'),
+    re_path(r'^oauth/', include('social_django.urls', namespace="social"))
 ]
